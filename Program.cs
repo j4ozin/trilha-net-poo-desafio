ceb5c27d48f4560aafba7771ddd1ba6c8d29bc67
João Basante <105830574+j4ozin@users.noreply.github.com>
2024-01-08 22:18:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ Nokia c1 = new Nokia("(81)9.5555-7777", "Nokia Tijolao", "333", 120);
 c1.Ligar();
 c1.InstalarAplicativo("Instagram");
 
-Console.WriteLine("--------------------------");
+Console.WriteLine("");
 
 Console.WriteLine("Smartphone Iphone:");
 Iphone c2 = new Iphone("(81)9.7777-7777", "Iphone 4s", "777", 800);
